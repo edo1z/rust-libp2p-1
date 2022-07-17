bc66e3e377a51990027a973bd085911b8d64c01f
web3ten0 <web3ten0@gmail.com>
2022-07-17 20:40:52 +0900

    PeerId作成

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use libp2p::{identity, PeerId};
+use std::error::Error;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("local key {:?}", local_key);
+    println!("local peer id {:?}", local_peer_id);
+    Ok(())
 }
